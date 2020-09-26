29ffe35a659e04a8134e22d8b23183593285c1fa
Nikolai Slavov <nslavov@alum.mit.edu>
2020-09-25 21:04:38 -0400

    Update google_track.js

--- google_track.js
+++ google_track.js
@@ -1,22 +1,9 @@
-<!--
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-4005755-3', 'auto', {'allowLinker': true});
-  ga('require', 'linker');
-  ga('linker:autoLink', ['slavovlab.net'] );
-  ga('send', 'pageview');
-  -->
 
 
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-4005755-5"></script>
-<script>
+async src="https://www.googletagmanager.com/gtag/js?id=UA-4005755-5">
+
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-4005755-5');
-</script>
