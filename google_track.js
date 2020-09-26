77925457ae12eb0ede45b591d6879468652258c6
Nikolai Slavov <nslavov@alum.mit.edu>
2020-09-25 21:09:22 -0400

    added google tracking code

--- google_track.js
+++ google_track.js
@@ -1,7 +1,7 @@
 
 
-async src="https://www.googletagmanager.com/gtag/js?id=UA-4005755-5">
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
