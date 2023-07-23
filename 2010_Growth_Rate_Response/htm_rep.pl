f3e533294ae315ae0db4d17ec165167e3937a983
Nikolai Slavov <nslavov@alum.mit.edu>
2023-07-23 13:40:52 -0400

    http --> https

--- 2010_Growth_Rate_Response/htm_rep.pl
+++ 2010_Growth_Rate_Response/htm_rep.pl
@@ -8,28 +8,27 @@ $x = 'http://www.northeastern.edu';
 $x2 = 'http://alum.mit.edu/www/';
 $y = 'https://web.northeastern.edu';
 
-$x = 'edunslavov/';
-$y = 'edu/slavovlab/';
+$x = 'http://slavovlab.net/';
+$y = 'https://slavovlab.net/';
 
 
-$File_Num=0;     
-$Skipped_Files = 0; 
+$File_Num=0;
+$Skipped_Files = 0;
 foreach my $file (glob("*.html")){ 	++$File_Num;
-	
+
 	undef @Lines;
 	open IN,  $file;
 	while(<IN>){
-	
+
 	$_ =~ s/$x/$y/g;
 	push @Lines, $_
 	}
 	close( IN );
 	open OUT,  ">$file";
 	foreach ( @Lines ){
-	
+
 		print OUT "$_";
-	
+
 	}
 	close( OUT );
 }
-
